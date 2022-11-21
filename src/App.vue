6d6cde9cc8050cffc21216576ed994b50c6d4d55
RodenasS <rodenas.sut@gmail.com>
2022-11-22 00:53:27 +0200

    push

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+  <body>
+
+  <header >
+    <h1> Kryžiažodis </h1>
+  </header>
+  <div id="papildomas">
+    <table class="puzzletable" id="puzzletable"></table>
+    <table id="questions">
+      <tr>
+        <td><strong>Horizontalus klausimai:</strong></td><td><strong>Vertikalus klausimai:</strong></td>
+      </tr>
+      <tr>
+        <td>1. Objektiskai orientuota skriptu programavimo kalba</td><td>2. Ford modelis dalyvaves 1991-1997 ralyje</td>
+      </tr>
+      <tr>
+        <td>3. Vienas is populiariausiu italu automobiliu gamintojas</td><td>4. Viena is populiariausiu italijos lenktyniniu trasu</td>
+      </tr>
+      <tr>
+        <td>5. 1988 C grupes toyota lenktyninio automobilio modelis</td><td>6. Variklio agregatas priverstinai ipuciantis ora i varikli</td>
+      </tr>
+      <tr>
+        <td>7. Senos kartos elektrinis variklio ipurskimas</td><td>8. Tarptautinis vidaus degimo variklio pavadinimas</td>
+      </tr>
+      <tr>
+        <td>9. Maziausias ford modelis</td><td>10. Koks skystis reikalingas palaikant optimalia automobilio darbine temperatura</td>
+      </tr>
+    </table>
+    <EngineComponent></EngineComponent>
+  </div>
+  </body>
+</template>
+
+<script>
+import EngineComponent from './components/Engine-Component.vue'
+export default {
+  name: 'App',
+  components: {
+    EngineComponent,
+  },
+}
+</script>
+
+<style lang="scss">
+@import "./styles/styles.scss";
+</style>
---
+++ src/components/Engine-Component.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="entiretable">
+    <table>
+      <tr><td><input class="button" type="submit" value="Restartuoti"  v-on:click="this.RestartavimoFunkcija()"></td>
+        <td><input class="button" type="submit" value="Tikrinti atsakymus"  v-on:click="this.TikrinimoFunkcija()"></td>
+        <td><input class="button" type="submit" value="Rodyti atsakymus" v-on:click="this.PagalbosFunkcija()"></td> </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'EngineComponent',
+  mounted() {
+    this.KrovimoFunkcija()
+  },
+  data: function() {
+  },
+
+  methods: {
+
+    Numeriai(){
+      document.getElementById("pzl_3_1").placeholder = "1";
+      document.getElementById("pzl_1_6").placeholder = "2";
+      document.getElementById("pzl_6_1").placeholder = "3";
+      document.getElementById("pzl_3_8").placeholder = "4";
+      document.getElementById("pzl_8_1").placeholder = "5";
+      document.getElementById("pzl_3_10").placeholder = "6";
+      document.getElementById("pzl_10_1").placeholder = "7";
+      document.getElementById("pzl_5_4").placeholder = "8";
+      document.getElementById("pzl_9_9").placeholder = "9";
+      document.getElementById("pzl_3_2").placeholder = "10";
+    },
+    KrovimoFunkcija(){
+      let puzzleTable = document.getElementById("puzzletable");
+      this.puzzleData= this.PuzlesFunkcija();
+      for ( let i = 0; i < this.puzzleData.length ; i++ ) {
+        let row = puzzleTable.insertRow(-1);
+        let rowData = this.puzzleData[i];
+        for(let j = 0 ; j < rowData.length ; j++){
+          let cell = row.insertCell(-1);
+          if(rowData[j] != 0){
+            let pzlID = String('pzl' + '_' + i + '_' + j);
+            cell.innerHTML = '<input type="text" class="inputBox" maxlength="1" style="text-transform: uppercase" ' + 'id="' + pzlID + '">';
+          }else{
+            cell.style.background  = "#transparent";
+          }
+        }
+      }
+      this.Numeriai();
+    },
+    PuzlesFunkcija(){
+      return [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 'e', 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 's', 0, 0, 0, 0, 0],
+        [0, 'j', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't', 0],
+        [0, 0, 'u', 0, 0, 0, 'o', 0, 'm', 0, 'u', 0],
+        [0, 0, 's', 0, 'o', 0, 'r', 0, 'o', 0, 'r', 0],
+        [0, 'f', 'i', 'a', 't', 0, 't', 0, 'l', 0, 'b', 0],
+        [0, 0, 'n', 0, 't', 0, 0, 0, 'a', 0, 'i', 0],
+        [0, 'm', 'i', 'n', 'o', 'l', 't', 'a', 0, 0, 'n', 0],
+        [0, 0, 'm', 0, 0, 0, 0, 0, 0, 'k', 'a', 0],
+        [0, 'm', 'o', 'n', 'o', 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+    },
+    RestartavimoFunkcija(){
+      let puzzleTable = document.getElementById("puzzletable");
+      puzzleTable.innerHTML = '';
+      this.KrovimoFunkcija();
+    },
+    TikrinimoFunkcija(){
+      for ( let i = 0; i < this.puzzleData.length ; i++ ) {
+        let rowData = this.puzzleData[i];
+        for(let j = 0 ; j < rowData.length ; j++){
+          if(rowData[j] != 0){
+            let selectedInputTextElement = document.getElementById('pzl' + '_' + i + '_' + j);
+            if(selectedInputTextElement.value != this.puzzleData[i][j]){
+              selectedInputTextElement.style.backgroundColor = '#a83232';
+
+            }else{
+              selectedInputTextElement.style.backgroundColor = '#4ca832';
+            }
+          }
+        }
+      }
+    },
+    PagalbosFunkcija(){
+      for ( let i = 0; i < this.puzzleData.length ; i++ ) {
+        let rowData = this.puzzleData[i];
+        for(let j = 0 ; j < rowData.length ; j++){
+          if(rowData[j] != 0){
+            let selectedInputTextElement = document.getElementById('pzl' + '_' + i + '_' + j);
+            selectedInputTextElement.value = this.puzzleData[i][j];
+            selectedInputTextElement.style.backgroundColor = '#4ca832';
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
